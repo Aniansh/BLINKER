24b2a4af2633433fa5e1c7ee1288f195d6c0bcd3
Aniansh <aniansh@yahoo.com>
2016-12-20 00:01:27 +0530

    Changed digitalWrite to analogWrite

--- BLINKER.ino
+++ BLINKER.ino
@@ -1,9 +1,16 @@
 void setup()
 {
-	
+	pinMode(13, OUTPUT);
 }
 
 void loop()
 {
-
+	for(int i=10;i<1000;i++)
+	{
+		digitalWrite(13, HIGH);
+		delay(10);
+		digitalWrite(13,LOW);
+		delay(10);
+	}
+	
 }
